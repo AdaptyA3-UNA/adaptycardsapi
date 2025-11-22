e2b8669c683d30bbd47f3db794e3a62e5c150210
Isaque Roberto <isaquezulato@gmail.com>
2025-11-21 21:02:05 -0300

    Add initial AppDbContext class
    Introduces the AppDbContext class in the Data namespace as a placeholder for future database context implementation.

---
+++ src/Adapty.API/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adapty.API.Data
+{
+    public class AppDbContext
+    {
+        // Database context implementation would go here
+    }
+}
